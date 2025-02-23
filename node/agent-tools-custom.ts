c58724872bf333cb9746d7fe263ffdd31a7224e4
ginlon <ginlon5241@gmail.com>
2025-02-23 22:29:43 +0800

    feat: agent-tools-custom

--- node/agent-tools-custom.ts
+++ node/agent-tools-custom.ts
@@ -1,4 +1,4 @@
-import { DynamicTool } from "@langchain/core/tools";
+import { DynamicStructuredTool, DynamicTool } from "@langchain/core/tools";
 import { AlibabaTongyiEmbeddings } from "@langchain/community/embeddings/alibaba_tongyi";
 import path from "path";
 import { FaissStore } from "@langchain/community/vectorstores/faiss";
@@ -7,6 +7,11 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrievalChain } from "langchain/chains/retrieval";
+import z from "zod";
+import { Calculator } from "@langchain/community/tools/calculator";
+import { pull } from "langchain/hub";
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import { ChatAlibabaTongyi } from "@langchain/community/chat_models/alibaba_tongyi";
 
 dotenv.config({
   path: "../.env.local",
@@ -18,6 +23,12 @@ const chatOptions = {
   configuration: {
     baseURL: process.env.BASE_URL,
   },
+};
+// 将 chatOptions 修改为:
+const tongyiChatOptions = {
+  alibabaApiKey: process.env.Tongyi_API_KEY,
+  temperature: 0,
+  modelName: "qwen-plus",
 };
 
 const stringReverseTool = new DynamicTool({
@@ -38,6 +49,23 @@ async function loadVectorStore() {
   return vectorStore;
 }
 
+async function createToolAgentWithTool(tools) {
+  const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
+
+  const llm = new ChatAlibabaTongyi(tongyiChatOptions);
+  const agent = await createOpenAIToolsAgent({
+    llm,
+    tools,
+    prompt,
+  });
+
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+  return agentExecutor;
+}
+
 async function main() {
   const prompt =
     ChatPromptTemplate.fromTemplate(`将以下问题仅基于提供的上下文进行回答：
@@ -45,7 +73,7 @@ async function main() {
     {context}
 
     问题：{input}`);
-  const llm = new ChatOpenAI(chatOptions);
+  const llm = new ChatAlibabaTongyi(tongyiChatOptions);
 
   const documentChain = await createStuffDocumentsChain({
     llm,
@@ -59,6 +87,52 @@ async function main() {
     combineDocsChain: documentChain,
     retriever,
   });
+
+  const retrieverTool = new DynamicTool({
+    name: "get-qiu-answer",
+    func: async (input: string) => {
+      const res = await retrievalChain.invoke({
+        input,
+      });
+
+      return res;
+    },
+    description: "获取小说 《球状闪电》相关问题的答案",
+  });
+
+  // const dateDiffTool = new DynamicStructuredTool({
+  //   name: "date-difference-calculator",
+  //   description: "计算两个日期之间的天数差",
+  //   schema: z.object({
+  //     date1: z.string().describe("第一个日期，以YYYY-MM-DD格式表示"),
+  //     date2: z.string().describe("第二个日期，以YYYY-MM-DD格式表示"),
+  //   }),
+  //   func: async ({ date1, date2 }) => {
+  //     const d1 = new Date(date1);
+  //     const d2 = new Date(date2);
+  //     const difference = Math.abs(d2.getTime() - d1.getTime());
+  //     const days = Math.ceil(difference / (1000 * 60 * 60 * 24));
+  //     return days.toString();
+  //   },
+  // });
+
+  const tools = [retrieverTool, new Calculator()];
+
+  const agents = await createToolAgentWithTool(tools);
+
+  const res = await agents.invoke({
+    input: "小说球状闪电中量子玫瑰的情节",
+  });
+
+  //   const res = await agents.invoke({
+  //     input: "我有 17 个苹果，小明的苹果比我的三倍少 10 个，小明有多少个苹果？",
+  //   });
+
+  //   const res = await agents.invoke({
+  //     input: "今年是 2024 年，今年 5.1 和 10.1 之间有多少天？",
+  //   });
+
+  console.log(res);
 }
 
 main();
